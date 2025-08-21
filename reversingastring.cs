332daa4dbf8c38534870583a3b1efb9b715c092e
Mannan Tayal <mannantayal21@gmail.com>
2025-08-21 11:30:08 +0530

    Add files via upload

---
+++ reversingastring.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string reversed = "";
+
+        for (int i = input.Length - 1; i >= 0; i--)
+            reversed += input[i];
+
+        Console.WriteLine("Reversed string: " + reversed);
+        Console.WriteLine("This program is written and executed by Mannan Tayal (0231BCA047)");
+    }
+}
